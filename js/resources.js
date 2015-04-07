6253c91fe4eaed0e46d833de2342bec5c172f757
Renan Gonzalez-Sainz <raygs2000@gmail.com>
2015-04-07 15:53:15 -0700

    4/7/15

---
+++ js/resources.js
@@ -0,0 +1,35 @@
+game.resources = [
+    /* Graphics. 
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
+    {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    {name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png"},
+    {name: "sheet2", type: "image", src: "data/img/sheet2.png"},
+    {name: "player", type: "image", src: "data/img/orcSpear.png"},
+    {name: "sheet4", type: "image", src: "data/img/sheet4.png"},
+    {name: "tower", type: "image", src: "data/img/tower_round.svg.png"},
+    {name: "creep1", type: "image", src: "data/img/brainmonster.png"},
+    {name: "title", type: "image", src: "data/img/title.png"},
+    {name: "title-screen", type: "image", src: "data/img/Awesomenauts.png"},
+    /* Atlases 
+     * @example
+     * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+     */
+
+    /* Maps. 
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
+    {name: "level01", type: "tmx", src: "data/map/test3.tmx"}
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
+
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
+];
